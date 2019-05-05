6cd5bc16bef31eb4d0000824df13e5a1945df8e4
thinkreed <thinkreed2017@outlook.com>
2019-05-06 07:31:59 +0800

    add problem 42 trapping rain water r2

---
+++ algo/second/p42_trapping_rain_water.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = len(height) - 1
+
+        level = 0
+        water = 0
+
+        while left < right:
+            if height[left] < height[right]:
+                lower = height[left]
+                left += 1
+            else:
+                lower = height[right]
+                right -= 1
+
+            level = max(level, lower)
+            water += level - lower
+
+        return water
