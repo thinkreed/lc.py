e8d1bdb4052aff752eddda2bee56990f051da656
thinkreed <thinkreed2017@outlook.com>
2019-05-14 07:15:29 +0800

    add problem 62 unique paths r2

---
+++ algo/second/p62_unique_paths.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m == 0 or n == 0:
+            return 0
+
+        dp = [1] * n
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[j] = dp[j - 1] + dp[j]
+
+        return dp[-1]
