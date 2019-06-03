9ef62aa759ed68ba4ca447e6b8a55f6d2466e3d8
thinkreed <thinkreed2017@outlook.com>
2019-06-04 07:38:30 +0800

    append to p120

--- algo/second/p120_triangle.py
+++ algo/second/p120_triangle.py
@@ -1,14 +1,14 @@
-class Solution(object):
+class Solution:
     def minimumTotal(self, triangle):
         """
         :type triangle: List[List[int]]
         :rtype: int
         """
         n = len(triangle)
-        min_len = triangle[-1]
+        minlen = triangle[-1]
 
         for layer in range(n - 2, -1, -1):
             for i in range(layer + 1):
-                min_len[i] = min(min_len[i], min_len[i + 1] + triangle[layer][i])
+                minlen[i] = min(minlen[i], minlen[i + 1]) + triangle[layer][i]
 
-        return min_len[0]
+        return minlen[0]
